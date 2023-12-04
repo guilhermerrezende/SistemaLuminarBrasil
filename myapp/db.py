2c71a299eec64cf6e491dd91c4416ca201737a10
guilhermerrezende <guilhermerezdev@gmail.com>
2023-12-04 15:50:42 -0300

    Descrição do seu commit

--- myapp/db.py
+++ myapp/db.py
@@ -3,11 +3,12 @@ from psycopg2 import pool
 
 
 
-DATABASE = 'luminarbrasil'
+DATABASE = 'postgres'
 USER = 'postgres'
-PASSWORD = '217881'
-HOST = 'localhost'
+PASSWORD = '21788102'
+HOST = 'luminar-1.c1tcpi4qrlgu.us-east-1.rds.amazonaws.com'
 PORT = '5432'
+
 
 # Configurar pool de conexão
 connection_pool = psycopg2.pool.SimpleConnectionPool(
